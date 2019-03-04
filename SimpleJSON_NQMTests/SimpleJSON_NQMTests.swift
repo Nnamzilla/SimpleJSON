5eb983ebe9e29a43ff546cc1f00d0367b2bf4a94
Nnamdi Massally <nqm14a@dmc-207-11.local>
2019-03-04 08:17:35 -0600

    Initial Commit

---
+++ SimpleJSON_NQMTests/SimpleJSON_NQMTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleJSON_NQMTests.swift
+//  SimpleJSON_NQMTests
+//
+//  Created by Nnamdi Massally on 3/4/19.
+//  Copyright © 2019 Nnamdi Massally. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleJSON_NQM
+
+class SimpleJSON_NQMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
